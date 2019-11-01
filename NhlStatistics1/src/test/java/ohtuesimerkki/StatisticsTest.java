3d63834ab0eb4b5a835ec5aa07e7cbe0839c6114
Jan Lindholm <jan.lindholm@helsinki.fi>
2019-11-01 16:01:16 +0200

    Tests for tehtävä 16

---
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,67 @@
+package ohtuesimerkki;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StatisticsTest {
+
+    Reader readerStub = new Reader() {
+
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<>();
+
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri", "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+
+            return players;
+        }
+    };
+
+    Statistics stats;
+
+    @Before
+    public void setUp() {
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }
+
+    @Test
+    public void testTopScorers() {
+        String expName = "Gretzky";
+        String actName = stats.topScorers(1).get(0).getName();
+        assertEquals(expName, actName);
+    }
+
+    @Test
+    public void testSearch() {
+        String expPlayerName = "Semenko";
+        String actPlayerName = stats.search("Semenko").getName();
+        assertEquals(expPlayerName, actPlayerName);
+    }
+
+    @Test
+    public void testSearchNull() {
+        assertEquals(null, stats.search("asdasdasdas"));
+    }
+
+    @Test
+    public void testTeams() {
+        String expName = "Lemieux";
+        String actName = stats.team("PIT").get(0).getName();
+        assertEquals(expName, actName);
+    }
+
+    @Test
+    public void testNoSuchTeam() {
+        assertEquals(0, stats.team("asdasdasdsa").size());
+    }
+
+}
