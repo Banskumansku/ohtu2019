7786df07031b671005bfe33f3c98e783a06d3e56
Banskumansku <jan.lindholm@helsinki.fi>
2019-11-16 14:50:38 +0200

    1-3 done

--- ohtuViikko3/nhlreader/src/main/java/ohtu/Player.java
+++ ohtuViikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,6 +3,9 @@ package ohtu;
 
 public class Player {
     private String name;
+    private int goals;
+    private String team;
+    private int assists;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +15,40 @@ public class Player {
         return name;
     }
 
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getGoals() {
+        return this.goals;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return this.team;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    /**
+     * @return the assists
+     */
+    public int getAssists() {
+        return this.assists;
+    }
+
+    public int scores() {
+        return this.assists + this.goals;
+    }
+//Aleksander Barkov   FLA   2 + 15 = 17
     @Override
     public String toString() {
-        return name;
+        return name + "   " + team + "   " + goals + " + " + assists + " = " + scores();
     }
-      
+
 }
