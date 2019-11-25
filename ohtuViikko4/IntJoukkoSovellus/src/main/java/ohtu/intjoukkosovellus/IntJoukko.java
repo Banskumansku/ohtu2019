19f06d3fe3417b284b207312a4a0d3ef74aee7f3
Banskumansku <jan.lindholm@helsinki.fi>
2019-11-25 16:11:49 +0200

    week 4 teht 5 done

--- ohtuViikko4/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
+++ ohtuViikko4/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -1,10 +1,9 @@
-
 package ohtu.intjoukkosovellus;
 
 public class IntJoukko {
 
     public final static int KAPASITEETTI = 5, // aloitustalukon koko
-                            OLETUSKASVATUS = 5;  // luotava uusi taulukko on 
+            OLETUSKASVATUS = 5;  // luotava uusi taulukko on
     // näin paljon isompi kuin vanha
     private int kasvatuskoko;     // Uusi taulukko on tämän verran vanhaa suurempi.
     private int[] ljono;      // Joukon luvut säilytetään taulukon alkupäässä. 
@@ -12,9 +11,6 @@ public class IntJoukko {
 
     public IntJoukko() {
         ljono = new int[KAPASITEETTI];
-        for (int i = 0; i < ljono.length; i++) {
-            ljono[i] = 0;
-        }
         alkioidenLkm = 0;
         this.kasvatuskoko = OLETUSKASVATUS;
     }
@@ -24,67 +20,81 @@ public class IntJoukko {
             return;
         }
         ljono = new int[kapasiteetti];
-        for (int i = 0; i < ljono.length; i++) {
-            ljono[i] = 0;
-        }
         alkioidenLkm = 0;
         this.kasvatuskoko = OLETUSKASVATUS;
-
     }
-    
-    
+
+
     public IntJoukko(int kapasiteetti, int kasvatuskoko) {
         if (kapasiteetti < 0) {
-            throw new IndexOutOfBoundsException("Kapasitteetti väärin");//heitin vaan jotain :D
+            throw new IndexOutOfBoundsException("Kapasiteetti ei voi olla negatiivinen");
         }
         if (kasvatuskoko < 0) {
-            throw new IndexOutOfBoundsException("kapasiteetti2");//heitin vaan jotain :D
+            throw new IndexOutOfBoundsException("Kasvatuskoko ei voi olla negatiivinen");
         }
         ljono = new int[kapasiteetti];
-        for (int i = 0; i < ljono.length; i++) {
-            ljono[i] = 0;
-        }
         alkioidenLkm = 0;
         this.kasvatuskoko = kasvatuskoko;
 
     }
 
-    public boolean lisaa(int luku) {
-
-        int eiOle = 0;
-        if (alkioidenLkm == 0) {
-            ljono[0] = luku;
-            alkioidenLkm++;
-            return true;
-        } else {
+    public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {
+        IntJoukko z = new IntJoukko();
+        for (int i1 : a.toIntArray()) {
+            z.lisaa(i1);
         }
+        for (int i1 : b.toIntArray()) {
+            z.lisaa(i1);
+        }
+        return z;
+    }
+
+    public static IntJoukko leikkaus(IntJoukko a, IntJoukko b) {
+        IntJoukko y = new IntJoukko();
+        int[] aTaulu = a.toIntArray();
+        int[] bTaulu = b.toIntArray();
+        for (int i = 0; i < a.toIntArray().length; i++) {
+            for (int j = 0; j < b.toIntArray().length; j++) {
+                if (aTaulu[i] == bTaulu[j]) {
+                    y.lisaa(bTaulu[j]);
+                }
+            }
+        }
+        return y;
+
+    }
+
+
+    public static IntJoukko erotus(IntJoukko a, IntJoukko b) {
+        IntJoukko z = new IntJoukko();
+        for (int i1 : a.toIntArray()) {
+            z.lisaa(i1);
+        }
+        for (int i1 : b.toIntArray()) {
+            z.poista(i1);
+        }
+        return z;
+    }
+
+    public void lisaa(int luku) {
         if (!kuuluu(luku)) {
             ljono[alkioidenLkm] = luku;
             alkioidenLkm++;
-            if (alkioidenLkm % ljono.length == 0) {
-                int[] taulukkoOld = new int[ljono.length];
-                taulukkoOld = ljono;
-                kopioiTaulukko(ljono, taulukkoOld);
+            if (alkioidenLkm == ljono.length) {
+                int[] taulukkoOld = ljono;
                 ljono = new int[alkioidenLkm + kasvatuskoko];
                 kopioiTaulukko(taulukkoOld, ljono);
             }
-            return true;
         }
-        return false;
     }
 
     public boolean kuuluu(int luku) {
-        int on = 0;
         for (int i = 0; i < alkioidenLkm; i++) {
             if (luku == ljono[i]) {
-                on++;
+                return true;
             }
         }
-        if (on > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return false;
     }
 
     public boolean poista(int luku) {
@@ -118,10 +128,9 @@ public class IntJoukko {
 
     }
 
-    public int mahtavuus() {
+    public int getAlkioidenLkm() {
         return alkioidenLkm;
     }
-
 
     @Override
     public String toString() {
@@ -148,48 +157,5 @@ public class IntJoukko {
         }
         return taulu;
     }
-   
 
-    public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {
-        IntJoukko x = new IntJoukko();
-        int[] aTaulu = a.toIntArray();
-        int[] bTaulu = b.toIntArray();
-        for (int i = 0; i < aTaulu.length; i++) {
-            x.lisaa(aTaulu[i]);
-        }
-        for (int i = 0; i < bTaulu.length; i++) {
-            x.lisaa(bTaulu[i]);
-        }
-        return x;
-    }
-
-    public static IntJoukko leikkaus(IntJoukko a, IntJoukko b) {
-        IntJoukko y = new IntJoukko();
-        int[] aTaulu = a.toIntArray();
-        int[] bTaulu = b.toIntArray();
-        for (int i = 0; i < aTaulu.length; i++) {
-            for (int j = 0; j < bTaulu.length; j++) {
-                if (aTaulu[i] == bTaulu[j]) {
-                    y.lisaa(bTaulu[j]);
-                }
-            }
-        }
-        return y;
-
-    }
-    
-    public static IntJoukko erotus ( IntJoukko a, IntJoukko b) {
-        IntJoukko z = new IntJoukko();
-        int[] aTaulu = a.toIntArray();
-        int[] bTaulu = b.toIntArray();
-        for (int i = 0; i < aTaulu.length; i++) {
-            z.lisaa(aTaulu[i]);
-        }
-        for (int i = 0; i < bTaulu.length; i++) {
-            z.poista(bTaulu[i]);
-        }
- 
-        return z;
-    }
-        
 }
--- ohtuViikko4/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/Sovellus.java
+++ ohtuViikko4/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/Sovellus.java
@@ -4,107 +4,83 @@ import java.util.Scanner;
 
 public class Sovellus {
 
+    private static Scanner lukija = new Scanner(System.in);
     private static IntJoukko A, B, C;
 
-    private static String luku() {
-        Scanner lukija = new Scanner(System.in);
-        String luettu = lukija.nextLine();
-        return luettu;
-    }
-
     private static IntJoukko mikaJoukko() {
-        String luettu;
-        Scanner lukija = new Scanner(System.in);
-        luettu = luku();
         while (true) {
-            if (luettu.equals("A") || luettu.equals("a")) {
-                return A;
-            } else if (luettu.equals("B") || luettu.equals("b")) {
-                return B;
-            } else if (luettu.equals("C") || luettu.equals("c")) {
-                return C;
-            } else {
-                System.out.println("Virheellinen joukko! " + luettu);
-                System.out.print("Yritä uudelleen!");
-                luettu = luku();
+            String luettu = lukija.nextLine();
+            switch (luettu) {
+                case "A":
+                case "a":
+                    return A;
+                case "B":
+                case "b":
+                    return B;
+                case "C":
+                case "c":
+                    return C;
+                default:
+                    System.out.println("Virheellinen joukko! " + luettu);
+                    System.out.print("Yritä uudelleen!");
             }
         }
     }
 
     private static void lisaa() {
-        int lisLuku;
-        IntJoukko joukko;
-        Scanner lukija = new Scanner(System.in);
-        System.out.print("Mihin joukkoon? ");
-        joukko = mikaJoukko();
-        System.out.println("");
+        System.out.print("Mihin joukkoon? \n");
+        IntJoukko joukko = mikaJoukko();
         System.out.print("Mikä luku lisätään? ");
-        lisLuku = lukija.nextInt();
+        int lisLuku = lukija.nextInt();
         joukko.lisaa(lisLuku);
-        return;
-
     }
 
     private static void yhdiste() {
-        IntJoukko aJoukko, bJoukko, c;
-        System.out.print("1. joukko? ");
-        aJoukko = mikaJoukko();
-        System.out.print("2. joukko? ");
-        bJoukko = mikaJoukko();
-        c = IntJoukko.yhdiste(aJoukko, bJoukko);
+        IntJoukko[] joukot = kysy();
+        IntJoukko c = IntJoukko.yhdiste(joukot[0], joukot[1]);
         System.out.println("A yhdiste B = " + c.toString());
-        return;
     }
 
     private static void leikkaus() {
-        IntJoukko aJoukko, bJoukko, c;
-        System.out.print("1. joukko? ");
-        aJoukko = mikaJoukko();
-        System.out.print("2. joukko? ");
-        bJoukko = mikaJoukko();
-        c = IntJoukko.leikkaus(aJoukko, bJoukko);
+        IntJoukko[] joukot = kysy();
+        IntJoukko c = IntJoukko.leikkaus(joukot[0], joukot[1]);
         System.out.println("A leikkaus B = " + c.toString());
-        return;
     }
 
     private static void erotus() {
-        IntJoukko aJoukko, bJoukko, c;
-        System.out.print("1. joukko? ");
-        aJoukko = mikaJoukko();
-        System.out.print("2. joukko? ");
-        bJoukko = mikaJoukko();
-        c = IntJoukko.erotus(aJoukko, bJoukko);
+        IntJoukko[] joukot = kysy();
+        IntJoukko c = IntJoukko.erotus(joukot[0], joukot[1]);
         System.out.println("A erotus B = " + c.toString());
-        return;
+    }
+
+    private static IntJoukko[] kysy() {
+        IntJoukko[] joukot = new IntJoukko[2];
+        System.out.print("1. joukko? ");
+        joukot[0] = mikaJoukko();
+        System.out.print("2. joukko? ");
+        joukot[1] = mikaJoukko();
+        return joukot;
     }
 
     private static void poista() {
-        IntJoukko joukko;
-        int lisLuku;
-        Scanner lukija = new Scanner(System.in);
         System.out.print("Mistä joukosta? ");
-        joukko = mikaJoukko();
+        IntJoukko joukko = mikaJoukko();
         System.out.print("Mikä luku poistetaan? ");
-        lisLuku = lukija.nextInt();
+        int lisLuku = lukija.nextInt();
         joukko.poista(lisLuku);
         return;
     }
 
     private static void kuuluu() {
-        IntJoukko joukko;
-        int kysLuku;
-        Scanner lukija = new Scanner(System.in);
         System.out.print("Mihin joukkoon? ");
-        joukko = mikaJoukko();
+        IntJoukko joukko = mikaJoukko();
         System.out.print("Mikä luku? ");
-        kysLuku = lukija.nextInt();
-        boolean kuuluuko = joukko.kuuluu(kysLuku);
-        if (kuuluuko) {
+        int kysLuku = lukija.nextInt();
+        if (joukko.kuuluu(kysLuku)) {
             System.out.println(kysLuku + " kuuluu joukkoon ");
         } else {
             System.out.println(kysLuku + " ei kuulu joukkoon ");
         }
-        return;
     }
 
     public static void main(String[] args) {
@@ -118,34 +94,42 @@ public class Sovellus {
         System.out.println("Komennot ovat lisää(li), poista(p), kuuluu(k), yhdiste(y), erotus(e), leikkaus(le) ja lopetus(quit)(q).");
         System.out.println("Joukon nimi komentona tarkoittaa pyyntöä tulostaa joukko.");
 
-        Scanner lukija = new Scanner(System.in);
         while (true) {
             luettu = lukija.nextLine();
-            if (luettu.equals("lisää") || luettu.equals("li")) {
-                lisaa();
-            } else if (luettu.equalsIgnoreCase("poista") || luettu.equalsIgnoreCase("p")) {
-                poista();
-            } else if (luettu.equalsIgnoreCase("kuuluu") || luettu.equalsIgnoreCase("k")) {
-                kuuluu();
-            } else if (luettu.equalsIgnoreCase("yhdiste") || luettu.equalsIgnoreCase("y")) {
-                yhdiste();
-            } else if (luettu.equalsIgnoreCase("leikkaus") || luettu.equalsIgnoreCase("le")) {
-                leikkaus();
-            } else if (luettu.equalsIgnoreCase("erotus") || luettu.equalsIgnoreCase("e")) {
-                erotus();
-            } else if (luettu.equalsIgnoreCase("A")) {
-                System.out.println(A);
-            } else if (luettu.equalsIgnoreCase("B")) {
-                System.out.println(B);
-            } else if (luettu.equalsIgnoreCase("C")) {
-                System.out.println(C);
-            } else if (luettu.equalsIgnoreCase("lopeta") || luettu.equalsIgnoreCase("quit") || luettu.equalsIgnoreCase("q")) {
-                System.out.println("Lopetetaan, moikka!");
-                break;
-            } else {
-                System.out.println("Virheellinen komento! " + luettu);
-                System.out.println("Komennot ovat lisää(li), poista(p), kuuluu(k), yhdiste(y), erotus(e) ja leikkaus(le).");
+            switch (luettu) {
+                case "A":
+                    System.out.println(A);
+                case "B":
+                    System.out.println(B);
+                case "C":
+                    System.out.println(C);
+                case "poista":
+                case "p":
+                    poista();
+                case "kuuluu":
+                case "k":
+                    kuuluu();
+                case "yhdiste":
+                case "y":
+                    yhdiste();
+                case "leikkaus":
+                case "le":
+                    leikkaus();
+                case "erotus":
+                case "e":
+                    erotus();
+                case "lopeta":
+                case "quit":
+                    System.out.println("Lopetetaan, moikka!");
+                    return;
+                case "lisää":
+                case "li":
+                    lisaa();
+                default:
+                    System.out.println("Virheellinen komento! " + luettu);
+                    System.out.println("Komennot ovat lisää(li), poista(p), kuuluu(k), yhdiste(y), erotus(e) ja leikkaus(le).");
             }
+
             System.out.println("Komennot ovat lisää(li), poista(p), kuuluu(k), yhdiste(y), erotus(e) ja leikkaus(le).");
         }
     }
